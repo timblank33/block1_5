993f6725d61cb326980bffb81db4da33057d77cf
Artyom Aksenov <timblank@gmail.com>
2023-05-30 21:59:19 +0300

    re

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 let imagesScr = [
-    '../img/Bitmap.png',
-    '../img/Bitmap_1.png',
-    '../img/Bitmap_2.png',
-    '../img/Bitmap_3.png',
-    '../img/Bitmap_4.png',
-    '../img/Bitmap_5.png',
-    '../img/Bitmap_6.png',
-    '../img/Bitmap_7.png',
-    '../img/Bitmap_1.png',
-    '../img/Bitmap_2.png',
-    '../img/Bitmap_3.png'
+    '/img/Bitmap.png',
+    '/img/Bitmap_1.png',
+    '/img/Bitmap_2.png',
+    '/img/Bitmap_3.png',
+    '/img/Bitmap_4.png',
+    '/img/Bitmap_5.png',
+    '/img/Bitmap_6.png',
+    '/img/Bitmap_7.png',
+    '/img/Bitmap_1.png',
+    '/img/Bitmap_2.png',
+    '/img/Bitmap_3.png'
 ]
 
 let brandLogo = document.querySelector('.brand__logo');
